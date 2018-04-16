7f6985db61948a6b71419c9f51fea3630e793d98
marcialwushu <marcialwushu@gmail.com>
2018-04-15 21:06:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+var WiFiControl = require('wifi-control');
+ 
+  //  Initialize wifi-control package with verbose output 
+  WiFiControl.init({
+    debug: true
+  });
+ 
+  //  Try scanning for access points: 
+  WiFiControl.scanForWiFi( function(err, response) {
+    if (err) console.log(err);
+    console.log(response);
+  });
